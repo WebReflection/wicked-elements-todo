9320e265bc537c5817a52ba1594130d1c2994dcb
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-05-19 12:41:25 +0200

    minor optimizations

--- esm/index.js
+++ esm/index.js
@@ -11,27 +11,61 @@ wickedElements.define('.todoapp', {
   init(event) {
     this.el = event.currentTarget;
     this.list = this.el.querySelector('.todo-list');
+    this.initStorage();
+    const hash = location.hash.slice(2);
+    if (hash)
+      this.el.querySelector(`a[href="#/${hash}"]`).click();
   },
 
-  add(value) {
+  initStorage() {
+    const id = this.el.id || '';
+
+    const todos = localStorage.getItem('todos#' + id);
+    if (todos)
+      JSON.parse(todos).forEach(
+        info => this.add(info.textContent, info.className)
+      );
+
+    addEventListener('beforeunload', () => {
+      const todos = [];
+      for (let
+        label,
+        labels = this.list.querySelectorAll('label'),
+        i = 0; i < labels.length; i++
+      ) {
+        label = labels[i];
+        todos[i] = {
+          textContent: label.textContent,
+          className: label.closest('li').className
+        };
+      }
+      localStorage.setItem('todos#' + id, JSON.stringify(todos));
+    });
+  },
+
+  add(textContent, className) {
     const li = document.createElement('li');
+    const checked = className === 'completed' ? 'checked' : '';
+    li.className = className || '';
     li.innerHTML = `
     <div class="view">
-      <input class="toggle" type="checkbox">
+      <input class="toggle" type="checkbox" ${checked}>
       <label></label>
       <button class="destroy"></button>
     </div>`;
-    li.querySelector('label').textContent = value;
+    li.querySelector('label').textContent = textContent;
     this.list.appendChild(li);
   },
+
   clearCompleted() {
     for (let
-      inputs = this.list.querySelectorAll('input:checked'),
-      i = 0; i < inputs.length; i++
+      lis = this.list.querySelectorAll('li.completed'),
+      i = 0; i < lis.length; i++
     ) {
-      inputs[i].closest('li').remove();
+      lis[i].remove();
     }
   },
+
   toggleAll(checked) {
     for (let
       inputs = this.list.querySelectorAll('input'),
@@ -40,6 +74,7 @@ wickedElements.define('.todoapp', {
       this.update(inputs[i], checked);
     }
   },
+
   update(input, checked) {
     input.checked = checked;
     input.closest('li').classList.toggle('completed', checked);
@@ -61,6 +96,7 @@ wickedElements.define('.todoapp', {
         break;
     }
   },
+
   onclick(event) {
     const {target} = event;
     switch (target.className) {
